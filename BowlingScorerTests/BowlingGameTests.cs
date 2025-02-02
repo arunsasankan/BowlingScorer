bfb779dccf1e7932b880580589be84da4d73b8a9
Arun Sasankan <sasankan.arun@yahoo.in>
2025-02-02 11:53:06 +0530

    Added some missing documentation

--- BowlingScorerTests/BowlingGameTests.cs
+++ BowlingScorerTests/BowlingGameTests.cs
@@ -4,9 +4,15 @@ using System.Collections.Generic;
 
 namespace BowlingScorer.Tests
 {
+    /// <summary>
+    /// Contains unit tests for the BowlingGame class.
+    /// </summary>
     [TestClass]
     public class BowlingGameTests
     {
+        /// <summary>
+        /// Tests that a perfect game (all strikes) returns a score of 300.
+        /// </summary>
         [TestMethod]
         public void CalculateScore_AllStrikes_ReturnsPerfectGameScore()
         {
@@ -17,10 +23,10 @@ namespace BowlingScorer.Tests
                 game.AddFrame(new Frame { FirstRoll = 10 });
             }
             game.AddBonusRolls(new List<Frame>
-            {
-                new Frame { FirstRoll = 10 },
-                new Frame { FirstRoll = 10 }
-            });
+                {
+                    new Frame { FirstRoll = 10 },
+                    new Frame { FirstRoll = 10 }
+                });
 
             // Act
             int score = game.CalculateScore();
@@ -29,6 +35,9 @@ namespace BowlingScorer.Tests
             Assert.AreEqual(300, score);
         }
 
+        /// <summary>
+        /// Tests that a game with all rolls knocking down one pin returns a score of 20.
+        /// </summary>
         [TestMethod]
         public void CalculateScore_AllOneGame_ReturnsScore20()
         {
@@ -46,6 +55,9 @@ namespace BowlingScorer.Tests
             Assert.AreEqual(20, score);
         }
 
+        /// <summary>
+        /// Tests that a spare followed by a roll of 3 returns a score of 16.
+        /// </summary>
         [TestMethod]
         public void CalculateScore_SpareFollowedBy3_Returns16()
         {
@@ -66,6 +78,9 @@ namespace BowlingScorer.Tests
             Assert.AreEqual(16, score);
         }
 
+        /// <summary>
+        /// Tests that a strike followed by rolls of 3 and 4 returns a score of 24.
+        /// </summary>
         [TestMethod]
         public void CalculateScore_StrikeFollowedBy3and4_Returns24()
         {
@@ -86,6 +101,9 @@ namespace BowlingScorer.Tests
             Assert.AreEqual(24, score);
         }
 
+        /// <summary>
+        /// Tests that a game with all gutter balls returns a score of 0.
+        /// </summary>
         [TestMethod]
         public void CalculateScore_GutterGame_ReturnsZero()
         {
@@ -103,6 +121,9 @@ namespace BowlingScorer.Tests
             Assert.AreEqual(0, score);
         }
 
+        /// <summary>
+        /// Tests that a game with all spares and a bonus roll of 5 returns a score of 150.
+        /// </summary>
         [TestMethod]
         public void CalculateScore_AllSparesWithFive_ReturnsScore150()
         {
@@ -121,6 +142,9 @@ namespace BowlingScorer.Tests
             Assert.AreEqual(150, score);
         }
 
+        /// <summary>
+        /// Tests that a game with no marks (strikes or spares) returns the sum of all pins knocked down.
+        /// </summary>
         [TestMethod]
         public void CalculateScore_NoMarks_ReturnsSumOfPins()
         {
@@ -138,6 +162,9 @@ namespace BowlingScorer.Tests
             Assert.AreEqual(70, score);
         }
 
+        /// <summary>
+        /// Tests that a random game returns the correct score.
+        /// </summary>
         [TestMethod]
         public void CalculateScore_RandomGame_ReturnsCorrectScore()
         {
@@ -155,10 +182,10 @@ namespace BowlingScorer.Tests
             game.AddFrame(new Frame { FirstRoll = 10 }); // Strike
             game.AddFrame(new Frame { FirstRoll = 10 }); // Strike
             game.AddBonusRolls(new List<Frame>
-            {
-                new Frame { FirstRoll = 8 },
-                new Frame { FirstRoll = 1 }
-            });
+                {
+                    new Frame { FirstRoll = 8 },
+                    new Frame { FirstRoll = 1 }
+                });
 
             // Act
             int score = game.CalculateScore();
@@ -167,6 +194,9 @@ namespace BowlingScorer.Tests
             Assert.AreEqual(167, score);
         }
 
+        /// <summary>
+        /// Tests that a spare in the last frame with a bonus roll returns the correct score.
+        /// </summary>
         [TestMethod]
         public void CalculateScore_SpareInLastFrameWithBonus_ReturnsCorrectScore()
         {
@@ -186,6 +216,9 @@ namespace BowlingScorer.Tests
             Assert.AreEqual(96, score);
         }
 
+        /// <summary>
+        /// Tests that a strike in the last frame with bonus rolls returns the correct score.
+        /// </summary>
         [TestMethod]
         public void CalculateScore_StrikeInLastFrameWithBonuses_ReturnsCorrectScore()
         {
@@ -197,10 +230,10 @@ namespace BowlingScorer.Tests
             }
             game.AddFrame(new Frame { FirstRoll = 10 }); // Strike in last frame
             game.AddBonusRolls(new List<Frame>
-            {
-                new Frame { FirstRoll = 10 },
-                new Frame { FirstRoll = 10 }
-            });
+                {
+                    new Frame { FirstRoll = 10 },
+                    new Frame { FirstRoll = 10 }
+                });
 
             // Act
             int score = game.CalculateScore();
@@ -209,6 +242,9 @@ namespace BowlingScorer.Tests
             Assert.AreEqual(111, score);
         }
 
+        /// <summary>
+        /// Tests that a mix of strikes and spares returns the correct score.
+        /// </summary>
         [TestMethod]
         public void CalculateScore_MixOfStrikesAndSpares_ReturnsCorrectScore()
         {
