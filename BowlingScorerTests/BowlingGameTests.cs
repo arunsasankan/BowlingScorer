8dc643c210ed6244c93a27d6ffa2f3ff370a4c38
Arun Sasankan <sasankan.arun@yahoo.in>
2025-02-02 07:48:28 +0530

    Created a unit testing project

---
+++ BowlingScorerTests/BowlingGameTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BowlingScorer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingScorer.Tests
+{
+    [TestClass()]
+    public class BowlingGameTests
+    {
+        [TestMethod()]
+        public void CalculateScoreTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void ReadDataTest()
+        {
+
+        }
+    }
+}
